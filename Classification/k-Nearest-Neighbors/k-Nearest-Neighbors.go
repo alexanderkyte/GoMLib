e0ce89c28d2c6daf54b00e51938673e3d73b6e36
Alexander Kyte <alexmkyte@gmail.com>
2013-10-01 21:40:50 -0400

    wrote most of k-nearest I think

--- Classification/k-Nearest-Neighbors/k-Nearest-Neighbors.go
+++ Classification/k-Nearest-Neighbors/k-Nearest-Neighbors.go
@@ -1,9 +1,59 @@
 package KNearestNeighbors
 
-type KComparable {
-  YieldComparing() []float64
+import (
+	"math"
+)
+
+type KComparable interface {
+	YieldComparing() []float64
+	YieldLabel() String
 }
 
-func yieldType(x KComparable, xs []KComparable) KComparable {
-  
+// xs must be at least 2 items
+func getLabel(x KComparable, xs []KComparable) KComparable {
+	distanceToLabel := make(map[float64]string)
+	distances := new([len(xs)]float64)
+	xFields := x.YieldComparing()
+
+	for i := 0; i < len(xs); i++ {
+		distance := float64(0)
+		xthFields := xs[i].YieldComparing()
+
+		for j := 0; j < len(xFields); j++ {
+			distance += math.Pow((xFields[j] - xthFields[j]), 2)
+		}
+
+		distance = math.Sqrt(distance)
+		distances[i] = distance
+		distanceToLabel[distance] = xs[i].YieldLabel()
+	}
+
+	sort.Sort(distances)
+
+	var max float64
+	var labelGuess string
+
+	{
+		labelCount = make(map[string]float)
+		step := (distances[1] - distances[0])
+		i := 0
+		last := distances[0]
+
+		for last-distances[i] < (step*5) && i < math.Floor(len(distances)/10) {
+			label := distanceToLabel[distances[i]]
+			if labelCount[label] {
+				labelCount[label] += 1
+			} else {
+				labelCount[label] = 0
+			}
+			if labelCount[label] > max {
+				max = labelCount[label]
+				labelGuess = label
+			}
+			last = distances[i]
+			i++
+		}
+	}
+	return labelGuess
+
 }
